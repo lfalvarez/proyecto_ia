ec76274ad427664a1c3b392a462315acecd23192
Felipe Alvarez <luisfelipealvarez@gmail.com>
2020-01-19 00:02:25 -0300

    Haciendo funcionar la cosa

--- base.py
+++ base.py
@@ -1,6 +1,6 @@
 import csv
 import datetime
-from collections import OrderedDict
+from copy import copy
 
 
 class Record:
@@ -21,7 +21,7 @@ class Record:
         self.stop_time = datetime.datetime.strptime(stop_time, '%Y-%m-%d %H:%M:%S.%f')
 
     def __repr__(self):
-        representation = '{access_id}, {start_node_str}, {start_node_id}, {end_node_str}, {end_node_id}, {start_time}, {stop_time}'
+        representation = 'Record("{access_id}", "{start_node_str}", "{start_node_id}", "{start_time}", "{stop_time}" , "{end_node_str}", "{end_node_id}")'
         return representation.format(access_id=self.access_id,
                                      start_node_str=self.start_node_str,
                                      start_node_id=self.start_node_id,
@@ -29,6 +29,21 @@ class Record:
                                      end_node_id=self.end_node_id,
                                      start_time=self.start_time,
                                      stop_time=self.stop_time)
+
+def valida(query, nodos_para_validar):
+    nodos_para_validar = list(reversed(nodos_para_validar))
+    query = list(reversed(query))
+    index = 0
+    for nodoq in query:
+        if nodoq != nodos_para_validar[index]:
+            return False
+        else:
+            if len(nodos_para_validar) == index + 1:
+                continue
+            if nodos_para_validar[index + 1] == nodoq:
+                index += 1
+        index += 1
+    return nodos_para_validar[index:][0] == nodos_para_validar[-1]
 
 
 class Ruta:
@@ -44,24 +59,42 @@ class Ruta:
         diff = ultima_ruta.start_time - primera_ruta.start_time
         return diff
 
-    def valida_que_nodo_sigue_en_ruta(self, index, nodos_actuales, nodo_a_comprobar):
+    def valida_que_nodo_sigue_en_ruta(self, index, nodos_actuales, nodo_a_comprobar, cantidad_de_nodos_par):
+        es_impar = bool(index % 2)
         min = index - 1 if (index - 1) >= 0 else index
-        max = index + 1 if (index) < len(nodos_actuales) else index
-        subset_de_nodos = nodos_actuales[min:max + 1]
-        if nodo_a_comprobar not in subset_de_nodos:
-            return False
-        return True
+        max = index + 1 if (index) < len(nodos_actuales) or es_impar else index
+        if es_impar:
+            subset_1 = nodos_actuales[min:max]
+            subset_2 = nodos_actuales[max: max+2]
+            result = nodo_a_comprobar in subset_1 and nodo_a_comprobar in subset_2
+            return result
+        factor = 1 if index > 0 else 0
+        sumando = 1 if cantidad_de_nodos_par else 2
+        min = index + sumando * factor
+        max = min +2
+        subset_de_nodos = nodos_actuales[min:max]
+        result = nodo_a_comprobar in subset_de_nodos
+        ##print(min, max, subset_de_nodos, sumando, cantidad_de_nodos_par)
+
+        return result
 
     def pasa_por(self, nodos):
         nodos_actuales = []
         for nodo in self.records:
             nodos_actuales.append(nodo.start_node_str)
             nodos_actuales.append(nodo.end_node_str)
+        cantidad_de_nodos_par = not bool(len(nodos) % 2)
         for index, nodo in enumerate(nodos):
-            if not self.valida_que_nodo_sigue_en_ruta(index, nodos_actuales, nodo):
+            if not self.valida_que_nodo_sigue_en_ruta(index, nodos_actuales, nodo, cantidad_de_nodos_par):
                 return False
         ## Esto es para el último salto que no está incluido dentro del loop
-        return self.valida_que_nodo_sigue_en_ruta(index, nodos_actuales, nodo)
+        return self.valida_que_nodo_sigue_en_ruta(index, nodos_actuales, nodo, cantidad_de_nodos_par)
+
+    def asegurar_orden_cronologico(self):
+        for i in range(len(self.records) - 1):
+            if self.records[i+1].start_time < self.records[i].start_time:
+                return False
+        return True
 
 def esta_ya_en_la_lista(lista, nodo):
     def compara(registro):
@@ -79,7 +112,7 @@ class ManagerDeRutas:
 
     def leer_registros(self):
         records = []
-        with open('access_chico.csv') as access:
+        with open('access.csv') as access:
             reader = csv.DictReader(access, delimiter=',')
             for row in reader:
                 access_id = row['access']
@@ -96,8 +129,7 @@ class ManagerDeRutas:
                            stop_time,
                            end_node_str,
                            end_node_id)
-                if not esta_ya_en_la_lista(records, r):
-                    records.append(r)
+                records.append(r)
 
         self.records = records
         return self.records
--- tests.py
+++ tests.py
@@ -1,13 +1,12 @@
 import unittest
-from base import Record, Ruta, ManagerDeRutas
+from base import Record, Ruta, ManagerDeRutas, valida
 
 
 class TestCases(unittest.TestCase):
     def setUp(self):
-        self.r1 = Record("0", "S2", "2", "2019-06-19 16:06:18.941", "2019-06-19 16:10:18.141", "CAL", "5")
-        self.r2 = Record("1", "S1", "1", "2019-06-19 16:17:24.550", "2019-06-19 16:29:10.882", "S2", "2")
-        self.r3 = Record("3", "S2", "2", "2019-06-19 16:31:24.550", "2019-06-19 16:33:10.882", "S1", "1")
-
+        self.r1 = Record("0", "CAL", "2", "2019-06-19 16:06:18.941", "2019-06-19 16:10:18.141", "S2", "5")
+        self.r2 = Record("1", "S2", "1", "2019-06-19 16:17:24.550", "2019-06-19 16:29:10.882", "S1", "2")
+        self.r3 = Record("3", "S1", "2", "2019-06-19 16:31:24.550", "2019-06-19 16:33:10.882", "S2", "1")
 
     def test_datetime_(self):
         self.assertEqual(self.r1.start_time.year, 2019)
@@ -30,30 +29,96 @@ class TestCases(unittest.TestCase):
         self.assertEqual(demora.seconds, 14 * 60)
 
     def test_ruta_valida(self):
-        por_donde_debe_pasar = ['CAL', 'S2', 'S1', 'S2'] ## Esto representa los nodos por donde debe pasar
+        por_donde_debe_pasar = ['CAL', 'S2', 'S1', 'S2']  ## Esto representa los nodos por donde debe pasar
         ## También es más o menos la ruta r1, r2 y r3
-        ruta = Ruta()
-        ruta.append(self.r1)
-        ruta.append(self.r2)
-        ruta.append(self.r3)
-        self.assertTrue(ruta.pasa_por(por_donde_debe_pasar))
+        ruta1 = Ruta([self.r1, self.r2, self.r3])
+        self.assertTrue(ruta1.pasa_por(por_donde_debe_pasar))
 
     def test_ruta_invalida(self):
-        por_donde_deberia_pasar = ['CAL', 'S2', 'S1', 'S3'] ## Esto representa los nodos por donde debe pasar
+        por_donde_deberia_pasar = ['CAL', 'S2', 'S1', 'S3']  ## Esto representa los nodos por donde debe pasar
         ## Esta ruta no es la misma que está ahí reflejada por las rutas r1, r2, r3
-        ruta = Ruta()
-        ruta.append(self.r1)
-        ruta.append(self.r2)
-        ruta.append(self.r3)
+        ruta = Ruta([self.r1, self.r2, self.r3])
         self.assertFalse(ruta.pasa_por(por_donde_deberia_pasar))
 
     def test_manager_de_rutas(self):
         manager = ManagerDeRutas()
         manager.leer_registros()
-        self.assertGreater(len(manager.records), 10) ## Hay más de 10 filas
+        self.assertGreater(len(manager.records), 10)  ## Hay más de 10 filas
         ruta = manager.obtenerRuta(['1', '4', '5', '7'])
         self.assertIsInstance(ruta, Ruta)
-        self.assertEqual(len(ruta.records), 4) ## Hay 4 registros del excel aquí
+        self.assertEqual(len(ruta.records), 4)  ## Hay 4 registros del excel aquí
+
+    def test_ruta_de_2(self):
+        records = [Record("33", "SCL", "1", "2019-06-19 21:36:29.906000", "2019-06-19 21:46:39.994000" , "S1", "3"),
+                   Record("35", "S1", "2", "2019-06-19 21:49:24.291000", "2019-06-19 22:01:10.613000" , "S2", "1"),
+                   Record("39", "S2", "1", "2019-06-19 22:36:47.065000", "2019-06-19 22:48:33.412000" , "S1", "2"),
+                   Record("43", "S1", "2", "2019-06-19 23:09:02.921000", "2019-06-19 23:20:41.941000" , "SCL", "3")]
+
+        ruta = Ruta(records)
+        self.assertTrue(ruta.pasa_por(['SCL', 'S1', 'S2', 'S1', 'SCL']))
+
+    def test_ruta_de_3(self):
+        records = [Record("33", "S1", "1", "2019-06-19 21:36:29.906000", "2019-06-19 21:46:39.994000" , "SCL", "3"),
+                   Record("34", "S2", "2", "2019-06-19 21:38:28.011000", "2019-06-19 21:44:48.755000" , "SCL", "3"),
+                   Record("35", "S2", "2", "2019-06-19 21:49:24.291000", "2019-06-19 22:01:10.613000" , "S1", "1"),
+                   Record("39", "S1", "1", "2019-06-19 22:36:47.065000", "2019-06-19 22:48:33.412000" , "S2", "2"),
+                   Record("43", "S2", "2", "2019-06-19 23:09:02.921000", "2019-06-19 23:20:41.941000" , "SCL", "3")]
+
+        ruta = Ruta(records)
+        self.assertFalse(ruta.pasa_por(['SCL', 'S1', 'SCL', 'S3', 'S1']))
+
+    def test_ruta_de_4(self):
+        records = [Record("33", "S1", "1", "2019-06-19 21:36:29.906000", "2019-06-19 21:46:39.994000" , "SCL", "3"),
+                   Record("63", "S2", "2", "2019-06-20 02:01:31.762000", "2019-06-20 02:11:31.676000" , "CAL", "5")]
+
+        ruta = Ruta(records)
+        self.assertFalse(ruta.pasa_por( ['SCL', 'S1', 'S2']))
+
+    def test_ruta_de_5(self):
+        records = [Record("33", "S1", "1", "2019-06-19 21:36:29.906000", "2019-06-19 21:46:39.994000" , "SCL", "3"),
+                   Record("63", "S1", "2", "2019-06-20 02:01:31.762000", "2019-06-20 02:11:31.676000" , "CAL", "5")]
+
+        ruta = Ruta(records)
+        self.assertFalse(ruta.pasa_por( ['SCL', 'S1', 'S2']))
+
+    def test_ruta_de_6(self):
+        records = [Record("33", "S1", "1", "2019-06-19 21:36:29.906000", "2019-06-19 21:46:39.994000" , "SCL", "3"),
+                   Record("62", "S1", "1", "2019-06-20 02:01:19.488000", "2019-06-20 02:12:51.201000" , "CAL", "5"),
+                   Record("63", "CAL", "2", "2019-06-20 02:01:31.762000", "2019-06-20 02:11:31.676000" , "S2", "5")]
+
+        ruta = Ruta(records)
+        self.assertFalse(ruta.pasa_por(['SCL', 'S1', 'S2', 'CAL']))
+
+    def test_asegurar_orden_cronologico_true(self):
+        records = [Record("33", "S1", "1", "2019-06-19 21:36:29.906000", "2019-06-19 21:46:39.994000" , "SCL", "3"),
+                   Record("62", "S1", "1", "2019-06-20 02:01:19.488000", "2019-06-20 02:12:51.201000" , "S2", "5"),
+                   Record("63", "S2", "2", "2019-06-20 02:01:31.762000", "2019-06-20 02:11:31.676000" , "CAL", "5")]
+
+        ruta = Ruta(records)
+        self.assertTrue(ruta.asegurar_orden_cronologico())
+
+    def test_asegurar_orden_cronologico_false(self):
+        records = [Record("33", "S1", "1", "2019-06-21 21:36:29.906000", "2019-06-19 21:46:39.994000" , "SCL", "3"),
+                   Record("62", "S1", "1", "2019-06-20 02:01:19.488000", "2019-06-20 02:12:51.201000" , "S2", "5"),
+                   Record("63", "S2", "2", "2019-06-20 02:01:31.762000", "2019-06-20 02:11:31.676000" , "CAL", "5")]
+
+        ruta = Ruta(records)
+        self.assertFalse(ruta.asegurar_orden_cronologico())
+
+    def test_valida_orden(self):
+        orden_malo = ['S1', 'SCL', 'S1', 'CAL', 'S1', 'S2', 'S2', 'CAL']
+        query = ['SCL', 'S1', 'S2', 'CAL']
+        self.assertFalse(valida(query, orden_malo))
+
+    def test_valida_orden2(self):
+        orden_bueno = ['S1', 'S2', 'S2', 'CAL', 'CAL', 'S3']
+        query = ['S1', 'S2', 'CAL','S3']
+        self.assertTrue(valida(query, orden_bueno))
+
+    def test_valida_orden3(self):
+        orden_malo = ['SCL', 'S2', 'S2', 'SCL', 'S2', 'CAL']
+        query = ['SCL', 'S2', 'CAL']
+        self.assertFalse(valida(query, orden_malo))
 
 if __name__ == '__main__':
     unittest.main()
